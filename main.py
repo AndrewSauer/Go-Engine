962716a0bda5063972a40e333dfef4ee17d1ca29
AndrewSauer <mrperson59@gmail.com>
2023-07-06 22:52:08 -0700

    Working on the ability to move
    This commit doesn't yet work lol

--- main.py
+++ main.py
@@ -14,7 +14,9 @@ img={}
 for k in img_keys:
     img[k]=pygame.image.load("./Sprites/"+k+".png")
 
-#contains all the info required to display the board
+#Rules constants
+SUICIDE_LEGAL=True
+SUPERKO="Positional"
 
 #prints a message to the top bar
 def display_message(message):
@@ -79,12 +81,11 @@ class Board:
         if self.position[tup[0]][tup[1]]==0 and (stone==1 or stone==2):
             alpha=128
             if stone==1:
-                ghost_stone_image=img['black'].convert_alpha()
+                ghost_stone_image=self.img['black'].convert_alpha()
             elif stone==2:
-                ghost_stone_image=img['white'].convert_alpha()
+                ghost_stone_image=self.img['white'].convert_alpha()
             ghost_stone_image.set_alpha(alpha)
-            screen.blit(ghost_stone_image,(self.x+x*self.space_length,self.y+y*self.space_length))              
-
+            screen.blit(ghost_stone_image,(self.x+tup[0]*self.space_length,self.y+tup[1]*self.space_length))              
 def blank_position(board_size):
     result=[]
     for i in range(board_size):
@@ -92,19 +93,59 @@ def blank_position(board_size):
         for j in range(board_size):
             result[i].append(0)
     return result
+
+#remove groups without liberties of color 1=black or 2=white
+def remove_surrounded(position,color):
+    grouped=position#grouped is 0 or 1 determining which stones have been put in a group so far
+    for i in range(len(grouped)):
+        for j in range(len(grouped[i])):
+            grouped[i][j]=0#an array of zeros the same size as position
+    new_position=position
+    for i in range(len(position)):
+        for j in range(len(position[i])):
+            if position[i][j]!=0 and grouped[i][j]==0:#if the stone hasn't been shown to be part of a group yet:
+                #Find the group, and remove it if no liberties
+
+#take in current board position, position of move, and player moving
+#return the board position after the move, or None if the move position is already occupied
+def move_with_capture(position,move_pos,player_turn):
+    new_position=position
+    new_position[move_pos[0]][move_pos[1]]=player_turn
+    new_position=remove_surrounded(new_position,3-player_turn)
+    new_position=remove_surrounded(new_position,3-player_turn)
+    return new_position
+
+#check if move is legal given history. If so return position after move, otherwise return None    
+def is_legal(history,position,move_pos,player_turn):
+    new_position=move_with_capture(position,move_pos,player_turn)
+    if new_position==None:
+        return None
+    if not SUICIDE_LEGAL and new_position[move_pos[0]][move_pos[1]]==0:
+        return None
+    if SUPERKO=="Positional" and new_position in history:
+        return None
+    return new_position
+
 board_size=19
 space_length=int((screen_height-top_bar_height)/board_size)
 board_length=board_size*space_length
 board_x=int((screen_width-board_length)/2)
 board_y=int((screen_height-top_bar_height-board_length)/2)+top_bar_height
 cur_board=Board(board_x,board_y,space_length,board_size)
-cur_board.position[0][0]=1
-cur_board.position[18][18]=2
+player_turn=1#black's turn first
+history=[cur_board.position]#history of all board positions
 while True:
     x,y=pygame.mouse.get_pos()
-    cur_board.display_board()
     mouse_board_pos=cur_board.mouse_over(x,y)
+    for event in pygame.event.get():#update when placing down stones
+        if event.type==pygame.MOUSEBUTTONDOWN and event.button==1:#Left mouse button is 1, right is 3, middle is 2
+            if mouse_board_pos:
+                new_position=is_legal(history,cur_board.position,mouse_board_pos,player_turn)
+                if after_position
+                    cur_board.position=new_position
+                    player_turn=3-player_turn
+                    history.append(cur_board.position)
+    cur_board.display_board()
     if mouse_board_pos:
-        cur_board.hover_stone(mouse_board_pos,2)
-    print("frame")
+        cur_board.hover_stone(mouse_board_pos,player_turn)
     pygame.display.flip()
